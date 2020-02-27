47ef09689de9c082c4037fb8424ec382f227c9ae
dannyozh <danielongzhihao@gmail.com>
2020-02-27 12:55:01 +0800

    added profile files to reducer and action so that it retrieves current user's profile

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,13 +2,27 @@
 // bring it in using redux state
 // pass it down to other components
 
-import React from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { getCurrentProfile } from "../../actions/profile";
 
-const Dashboard = props => {
+const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, []);
   return <div>Dashboard</div>;
 };
 
-Dashboard.propTypes = {};
+Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
+};
 
-export default Dashboard;
+const maptStateToProps = state => ({
+  auth: state.auth,
+  profile: state.profile
+});
+
+export default connect(maptStateToProps, { getCurrentProfile })(Dashboard);
