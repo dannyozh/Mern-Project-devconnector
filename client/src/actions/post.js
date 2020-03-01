bebb772731517d0e54402d234b2ec246963aae7b
dannyozh <danielongzhihao@gmail.com>
2020-03-01 13:15:37 +0800

    user can delete own posts

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from "./types";
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from "./types";
+import { set } from "mongoose";
 
 // Get posts
 export const getPosts = () => async dispatch => {
@@ -47,6 +48,26 @@ export const removeLike = id => async dispatch => {
       //   send id as well as likes
       payload: { id, likes: res.data }
     });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Delete post
+export const deletePost = id => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/posts/${id}`);
+
+    dispatch({
+      type: DELETE_POST,
+      //   send id as well as likes
+      payload: id
+    });
+
+    dispatch(setAlert("Post Removed", "success"));
   } catch (err) {
     dispatch({
       type: POST_ERROR,
