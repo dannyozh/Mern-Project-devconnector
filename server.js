14bc0757dc3d98162cc4656dbde484de9b6b3661
dannyozh <danielongzhihao@gmail.com>
2020-03-01 16:50:13 +0800

    modified server.js and package.json on server side to optimse for production

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const connectDB = require("./config/db");
 
 const app = express();
 
+const path = require("path");
+
 // Connect Database
 
 connectDB();
@@ -13,8 +15,6 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("API Running"));
-
 // accessing the express router, defining routes;
 
 app.use("/api/users", require("./routes/api/users"));
@@ -22,6 +22,15 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
